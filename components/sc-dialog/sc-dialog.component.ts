2a2fcb6a84044a680e71bc67b6972ec3269be3db
Payton <jmgarden@qq.com>
2021-04-01 08:45:46 +0800

    feat: one place for practice

---
+++ components/sc-dialog/sc-dialog.component.ts
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2018 Quest Software and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, Renderer2, TemplateRef } from '@angular/core';
+
+@Component({
+  selector: 'sc-dialog',
+  templateUrl: './sc-dialog.component.html',
+  styleUrls: ['./sc-dialog.component.scss'],
+})
+export class ScDialogComponent implements OnInit {
+  @Input() title: string;
+  @Input() footTemplate: TemplateRef<any>;
+  @Output() close = new EventEmitter<any>();
+  @Output() destroy = new EventEmitter<any>();
+
+  constructor(private eleRef: ElementRef, private render: Renderer2) {}
+
+  ngOnInit() {
+    this.render.listen(this.eleRef.nativeElement, 'transitionend', e => {
+      if (e.target === this.eleRef.nativeElement) {
+        this.render.addClass(this.eleRef.nativeElement, 'hideit');
+        this.destroy.emit();
+      }
+    });
+  }
+
+  onClose() {
+    this.doClose();
+  }
+
+  onKeyDown(event: KeyboardEvent) {
+    const which = event.which;
+    if (which === 27) {
+      this.doClose();
+    }
+  }
+
+  private doClose() {
+    this.render.addClass(this.eleRef.nativeElement, 'unvisible');
+    this.close.emit();
+  }
+}
---
+++ src/bubble.js
@@ -0,0 +1,23 @@
+function checkArray(array) {
+  return Array.isArray(array) && array.length > 2;
+}
+
+function swap(array, left, right) {
+  let rightValue = array[right];
+  array[right] = array[left];
+  array[left] = rightValue;
+}
+
+function bubble(arr) {
+  
+  for (let i = arr.length - 1; i > 0; i--) {
+    // 从 0 到 `length - 1` 遍历
+    for (let j = 0; j < i; j++) {
+      if (arr[j] < arr[j + 1]) swap(arr, j, j + 1);
+    }
+  }
+  console.log(arr);
+  return arr;
+}
+
+const a = bubble([10, 9]);
